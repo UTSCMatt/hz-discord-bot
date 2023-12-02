45a0bed8c38dcd5198a004407479593196fb855b
UTSCMatt <matthewyathin.lau@alum.utoronto.ca>
2023-12-02 17:19:35 -0500

    Add logging

--- hz-discord-bot/hz-discord-bot/Program.cs
+++ hz-discord-bot/hz-discord-bot/Program.cs
@@ -1,9 +1,14 @@
 using hz_discord_bot;
+using Serilog;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+    })
+    .UseSerilog((host, config) =>
+    {
+        config.ReadFrom.Configuration(host.Configuration);
     })
     .Build();
 
